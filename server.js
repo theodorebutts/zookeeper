0d98836120d6780f77b4ca29f9d14ed2a30d814f
Theodore Butts <theodorebutts@gmail.com>
2020-07-14 21:47:44 -0600

    add frontend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { animals } = require('./data/animals')
 const fs = require('fs')
 const path = require('path')
 
-
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -94,6 +94,18 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals)
     res.json(animal)
   }
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
 })
 
 app.listen(PORT, () => {
